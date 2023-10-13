9228900a9c5c09ee38164476259d03e2526a6414
DoanMinhHieu123321 <hieudoanpp9@gmail.com>
2023-10-13 14:35:06 +0700

    th66

---
+++ ThucHanh01/Utilities/Functions.cs
@@ -0,0 +1,13 @@
+﻿using SlugGenerator;
+
+namespace ThucHanh01.Utilities
+{
+    public class Functions
+    {
+        public static string TitleSLugGeneration(string type, string title, long id)
+        {
+            string sTitle = type + "-" + SlugGenerator.SlugGenerator.GenerateSlug(title) + "-" + id.ToString() + ".html";
+            return sTitle;
+        }
+    }
+}
